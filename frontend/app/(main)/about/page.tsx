6de1bd30cf43a43c1672704f54351e442e605404
dhirajsuthar781 <drsuthar781@gmail.com>
2026-01-07 19:17:26 +0530

    folder structure with some pages

---
+++ frontend/app/(main)/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+/*------------------------------------
+show data about this project and by whome this was created with what motive
+--------------------------------------*/
+
+export default function page({}: Props) {
+  return (
+    <div>About page</div>
+  )
+}
---
+++ frontend/app/(main)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function page() {
+     return (
+          <section className='   border p-2'>
+             Homepage
+          </section>
+     )
+}
